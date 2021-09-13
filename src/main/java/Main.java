b75aef21bdde148d68c5bef75241d4422359488e
k-laughner <74153467+k-laughner@users.noreply.github.com>
2021-09-13 16:15:16 -0400

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,30 +3,42 @@
  *  Copyright 2021 Kaylee Laughner
  */
 import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+    static Scanner console = new Scanner (System.in);
+    static public char restart = 'n';
+
     public static void main(String[] args) {
-        System.out.println("What is your height, in inches?");
-        Scanner input = new Scanner(System.in);
-        float height = input.nextFloat();
+      do {
+        try{
+          System.out.println("What is your height, in inches?");
+          float height = console.nextFloat();
+          
+          System.out.println("What is your weight, in pounds?");
+          float pounds = console.nextFloat();
 
-        System.out.println("What is your weight, in pounds?");
-        Scanner input2 = new Scanner(System.in);
-        float pounds = input2.nextFloat();
+          float bmi = (pounds/(height*height))*703;
 
-        float bmi = (pounds/(height*height))*703;
-
-        System.out.println("Your BMI is " + bmi);
+          System.out.println("Your BMI is " + bmi);
 
 
-        if (bmi>=18.5 && bmi <= 25){
-            System.out.println("You are within the ideal weight range.");
+          if (bmi>=18.5 && bmi <= 25){
+              System.out.println("You are within the ideal weight range.");
+          }
+          else if (bmi > 25) {
+              System.out.println("You are overweight. You should see your doctor.");
+          }
+          else {
+              System.out.println("You are underweight. You should see your doctor.");
+          }
         }
-        else if (bmi > 25) {
-            System.out.println("You are overweight. You should see your doctor.");
-        }
-        else {
-            System.out.println("You are underweight. You should see your doctor.");
+        catch (InputMismatchException fe){
+          System.out.println("You did not enter an integer. Please try again.");
+          restart='y';
+          console.next();
         }
     }
+    while (restart == 'y');
+  }
 }
